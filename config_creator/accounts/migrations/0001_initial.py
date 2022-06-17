ec38e3bd4e1b82ab0d2165532757f00065757cb5
sean-conkie <sean.conkie@sky.uk>
2022-06-17 20:20:18 +0100

    push linting

--- config_creator/accounts/migrations/0001_initial.py
+++ config_creator/accounts/migrations/0001_initial.py
@@ -7,24 +7,41 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='User',
+            name="User",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('password', models.CharField(max_length=128, verbose_name='password')),
-                ('last_login', models.DateTimeField(blank=True, null=True, verbose_name='last login')),
-                ('email', models.EmailField(max_length=255, unique=True, verbose_name='email address')),
-                ('is_active', models.BooleanField(default=True)),
-                ('admin', models.BooleanField(default=False)),
-                ('forename', models.TextField()),
-                ('surname', models.TextField()),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("password", models.CharField(max_length=128, verbose_name="password")),
+                (
+                    "last_login",
+                    models.DateTimeField(
+                        blank=True, null=True, verbose_name="last login"
+                    ),
+                ),
+                (
+                    "email",
+                    models.EmailField(
+                        max_length=255, unique=True, verbose_name="email address"
+                    ),
+                ),
+                ("is_active", models.BooleanField(default=True)),
+                ("admin", models.BooleanField(default=False)),
+                ("forename", models.TextField()),
+                ("surname", models.TextField()),
             ],
             options={
-                'abstract': False,
+                "abstract": False,
             },
         ),
     ]
--- config_creator/accounts/migrations/0002_user_staff.py
+++ config_creator/accounts/migrations/0002_user_staff.py
@@ -6,13 +6,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('accounts', '0001_initial'),
+        ("accounts", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='user',
-            name='staff',
+            model_name="user",
+            name="staff",
             field=models.BooleanField(default=True),
         ),
     ]
