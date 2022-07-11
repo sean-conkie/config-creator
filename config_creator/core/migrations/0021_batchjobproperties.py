f8cf901899e43fee7f0717cd6376dc71c1706c25
sean-conkie <sean.conkie@sky.uk>
2022-07-11 21:42:12 +0100

    python black linting

--- config_creator/core/migrations/0021_batchjobproperties.py
+++ config_creator/core/migrations/0021_batchjobproperties.py
@@ -7,16 +7,36 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0020_alter_jobtask_name'),
+        ("core", "0020_alter_jobtask_name"),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='BatchJobProperties',
+            name="BatchJobProperties",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('prefix', models.CharField(help_text='Enter the prefix which will be used for all tasks in this job; i.e. spine__order', max_length=255, verbose_name='Script Name Prefix')),
-                ('job', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to='core.job')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "prefix",
+                    models.CharField(
+                        help_text="Enter the prefix which will be used for all tasks in this job; i.e. spine__order",
+                        max_length=255,
+                        verbose_name="Script Name Prefix",
+                    ),
+                ),
+                (
+                    "job",
+                    models.OneToOneField(
+                        on_delete=django.db.models.deletion.CASCADE, to="core.job"
+                    ),
+                ),
             ],
         ),
     ]
