4feccae568e0971cc0c55a30f889b51c64faa73a
sean-conkie <sean.conkie@sky.uk>
2022-10-03 20:21:02 +0100

    lint migration

--- config_creator/core/migrations/0044_alter_batchcustomjobtaskproperties_sql_and_more.py
+++ config_creator/core/migrations/0044_alter_batchcustomjobtaskproperties_sql_and_more.py
@@ -7,21 +7,44 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0043_auto_20220927_2019'),
+        ("core", "0043_auto_20220927_2019"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='batchcustomjobtaskproperties',
-            name='sql',
-            field=models.CharField(help_text='Enter the name of the sql field to be used by this task', max_length=255, verbose_name='SQL Script Name'),
+            model_name="batchcustomjobtaskproperties",
+            name="sql",
+            field=models.CharField(
+                help_text="Enter the name of the sql field to be used by this task",
+                max_length=255,
+                verbose_name="SQL Script Name",
+            ),
         ),
         migrations.CreateModel(
-            name='TaskTypeToWriteDisposition',
+            name="TaskTypeToWriteDisposition",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('task_type', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='core.tasktype')),
-                ('write_disposition', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='core.writedisposition')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "task_type",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE, to="core.tasktype"
+                    ),
+                ),
+                (
+                    "write_disposition",
+                    models.ForeignKey(
+                        on_delete=django.db.models.deletion.CASCADE,
+                        to="core.writedisposition",
+                    ),
+                ),
             ],
         ),
     ]
