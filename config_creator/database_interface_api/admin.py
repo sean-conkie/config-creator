08f09c61470e2ffb2d15b38137fc86fcaad59af8
sean-conkie <sean.conkie@sky.uk>
2022-10-13 22:05:58 +0100

    update model for source tables

--- config_creator/database_interface_api/admin.py
+++ config_creator/database_interface_api/admin.py
@@ -1,4 +1,9 @@
+import re
+
 from .models import *
+from core.models import BigQueryDataType
+from database_interface_api.dbhelper import get_database_schema, get_schema
+from database_interface_api.views import get_connection
 from django.contrib import admin
 
 
@@ -9,7 +14,9 @@ class ConnectionAdmin(admin.ModelAdmin):
         "get_connectiontype",
         "get_username",
     ]
-    search_fields = ["email"]
+    search_fields = ["email", "name"]
+    actions = ["refresh"]
+    ordering = ["connectiontype", "name"]
 
     def get_connectiontype(self, obj):
         return obj.connectiontype.description
@@ -23,9 +30,145 @@ class ConnectionAdmin(admin.ModelAdmin):
     get_username.admin_order_field = "username"
     get_username.short_description = "User Name"
 
+    def refresh(self, request, queryset):
+        for connection in queryset:
+            schema = get_schema(
+                get_connection(
+                    connection.user_id,
+                    connection.id,
+                )
+            )
+
+            for dataset in schema.get("result", {}):
+                if not Dataset.objects.filter(
+                    connection=connection,
+                    name=dataset.get("name"),
+                ).exists():
+                    indb_dataset = Dataset(
+                        connection=connection,
+                        name=dataset.get("name"),
+                    )
+                else:
+                    indb_dataset = Dataset.objects.get(
+                        connection=connection,
+                        name=dataset.get("name"),
+                    )
+
+                indb_dataset.save()
+
+                # delete all tables for dataset - clear up db because
+                # we won't be able to tell if a table has been deleted
+                # by checking source.
+                Table.objects.filter(
+                    dataset=indb_dataset,
+                ).delete()
+
+                dataset_schema = get_database_schema(
+                    get_connection(
+                        connection.user_id,
+                        connection.id,
+                    ),
+                    dataset.get("name"),
+                )
+
+                for table in dataset_schema.get("result", {}):
+                    if not Table.objects.filter(
+                        dataset=indb_dataset,
+                        name=table.get("name"),
+                    ).exists():
+                        indb_table = Table(
+                            dataset=indb_dataset,
+                            name=table.get("name"),
+                        )
+                    else:
+                        indb_table = Table.objects.get(
+                            dataset=indb_dataset,
+                            name=table.get("name"),
+                        )
+
+                    indb_table.save()
+
+                    for column in table.get("content", []):
+                        m = re.search(
+                            r"^(\w+)(?:<.+)?$", column.get("data_type"), re.IGNORECASE
+                        )
+                        source_data_type = m.group(1).upper() if m else ""
+                        data_type = (
+                            BigQueryDataType.objects.get(name=source_data_type)
+                            if BigQueryDataType.objects.filter(
+                                name=source_data_type
+                            ).exists()
+                            else None
+                        )
+
+                        if not Column.objects.filter(
+                            table=indb_table,
+                            name=column.get("column_name"),
+                        ).exists():
+                            indb_column = Column(
+                                table=indb_table,
+                                name=column.get("column_name"),
+                                data_type=data_type,
+                                position=column.get("ordinal_position"),
+                                is_nullable=column.get("is_nullable"),
+                            )
+                        else:
+                            indb_column = Column.objects.get(
+                                table=indb_table,
+                                name=column.get("column_name"),
+                                data_type=data_type,
+                                position=column.get("ordinal_position"),
+                                is_nullable=column.get("is_nullable"),
+                            )
+
+                        indb_column.save()
+
+    refresh.short_description = "Refresh Connection Schema"
+
 
 @admin.register(ConnectionType)
 class ConnectionTypeAdmin(admin.ModelAdmin):
     list_display = [
         "description",
     ]
+
+
+@admin.register(Dataset)
+class DatasetAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "get_connection",
+    ]
+    list_filter = ["connection"]
+    search_fields = ["name"]
+    ordering = ["connection", "name"]
+
+    def get_connection(self, obj):
+        return obj.connection.name
+
+    get_connection.admin_order_field = "connection"
+    get_connection.short_description = "Connection Name"
+
+
+class FieldInLine(admin.TabularInline):
+    model = Column
+    extra = 1
+    max_num = 1
+
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "get_dataset",
+    ]
+    list_filter = ["dataset"]
+    search_fields = ["name"]
+    ordering = ["dataset", "name"]
+    inlines = [FieldInLine]
+
+    def get_dataset(self, obj):
+        return obj.dataset.name
+
+    get_dataset.admin_order_field = "dataset"
+    get_dataset.short_description = "Dataset Name"
--- config_creator/database_interface_api/models.py
+++ config_creator/database_interface_api/models.py
@@ -1,3 +1,4 @@
+from core.models import BigQueryDataType, DEFAULT_DATA_TYPE_ID
 from django.db import models
 from django.conf import settings
 from django.urls import reverse
@@ -7,6 +8,9 @@ User = settings.AUTH_USER_MODEL
 __all__ = [
     "ConnectionType",
     "Connection",
+    "Dataset",
+    "Table",
+    "Column",
 ]
 
 
@@ -16,6 +20,11 @@ class ConnectionType(models.Model):
 
     def __str__(self) -> str:
         return self.description
+
+    def todict(self):
+        return {
+            "description": self.description,
+        }
 
 
 class Connection(models.Model):
@@ -29,7 +38,7 @@ class Connection(models.Model):
         blank=True,
         unique=False,
         max_length=250,
-        verbose_name="Project Name",
+        verbose_name="Connection String",
         null=True,
     )
     credentials = models.FileField(
@@ -69,7 +78,6 @@ class Connection(models.Model):
     port = models.IntegerField(
         blank=True, unique=False, verbose_name="Port", null=True, default=0
     )
-
     schema = models.FileField(
         verbose_name="Schema File",
         upload_to=f"schema/",
@@ -83,3 +91,119 @@ class Connection(models.Model):
 
     def get_absolute_url(self):
         return reverse("connection-detail", kwargs={"pk": self.pk})
+
+    def todict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "credentials": self.credentials,
+            "connection_string": self.connectionstring,
+            "user_name": self.user_name,
+            "host": self.host,
+            "sid": self.sid,
+            "port": self.port,
+            "schema": self.schema,
+            "secret_key": self.secret_key,
+            "connection_type": self.connectiontype.todict(),
+        }
+
+
+class Dataset(models.Model):
+    connection = models.ForeignKey(
+        Connection,
+        on_delete=models.CASCADE,
+        verbose_name="Project",
+    )
+    name = models.CharField(
+        blank=False,
+        unique=False,
+        max_length=250,
+        verbose_name="Dataset Name",
+        null=False,
+    )
+
+    def __str__(self):
+        return self.name
+
+    def todict(self):
+        """
+        It takes an object of type `User` and returns a dictionary with the keys `id` and `name` and the
+        values of the object's `id` and `name` attributes
+
+        Returns:
+          A dictionary with the id and name of the object.
+        """
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+
+class Table(models.Model):
+    dataset = models.ForeignKey(
+        Dataset,
+        on_delete=models.CASCADE,
+        verbose_name="Dataset",
+    )
+    name = models.CharField(
+        blank=False,
+        unique=False,
+        max_length=250,
+        verbose_name="Table Name",
+        null=False,
+    )
+
+    def __str__(self):
+        return self.name
+
+    def todict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+
+
+class Column(models.Model):
+    table = models.ForeignKey(
+        Table,
+        on_delete=models.CASCADE,
+        verbose_name="Table",
+    )
+    name = models.CharField(
+        blank=False,
+        unique=False,
+        max_length=250,
+        verbose_name="Table Name",
+        null=False,
+    )
+    data_type = models.ForeignKey(
+        BigQueryDataType,
+        verbose_name="Data Type",
+        on_delete=models.SET_DEFAULT,
+        default=DEFAULT_DATA_TYPE_ID,
+        null=False,
+        blank=True,
+    )
+    position = models.IntegerField(
+        verbose_name="Ordinal Position",
+        null=False,
+        blank=True,
+        default=-1,
+    )
+    is_nullable = models.BooleanField(
+        verbose_name="Is Column Nullable",
+        default=True,
+    )
+
+    def __str__(self):
+        return self.name
+
+    def todict(self):
+        return {
+            "id": self.id,
+            "table": self.table.todict(),
+            "name": self.name,
+            "data_type": self.data_type,
+            "position": self.position,
+            "is_nullable": self.is_nullable,
+        }
